39965d2c844ebabe99b362dbcb92154d7f0397c3
Pradeep <pradeep@seleniumframework.com>
2017-06-09 19:24:54 -0400

    using fmt

--- src/github.com/machzqcq/init.go
+++ src/github.com/machzqcq/init.go
@@ -1,11 +1,15 @@
 package main
 
+import (
+	"fmt"
+)
+
 var (
 	message string = "Hello world" //use const for declaring constants
 )
 
 func main() {
-	println(message)
+	fmt.Println(message)
 }
 
 //init runs after variable initialization but before any other function runs
