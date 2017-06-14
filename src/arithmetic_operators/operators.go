0b46ae317b576e9b5b83b1555024ad89680c931a
Pradeep <pradeep@seleniumframework.com>
2017-06-14 16:54:06 -0400

    arithmetic operators

---
+++ src/arithmetic_operators/operators.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+	add := 1 + 2
+	println(add)
+
+	subtract := 5 - 3
+	println(subtract)
+
+	remainder := 5 % 2
+	println(remainder)
+
+	inc := 1
+	inc++
+	println(inc)
+
+	inc += 5
+	println(5)
+
+	inc *= 3
+	println(inc)
+
+}
